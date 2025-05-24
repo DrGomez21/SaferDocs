d3fae4b4678bcdb4e2002fd4faa4072691295961
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-24 19:44:57 -0300

    Agregar componente Explicacion con información sobre el uso y validez del documento

---
+++ src/components/Explicacion.jsx
@@ -0,0 +1,12 @@
+export const Explicacion = () => {
+  return (
+    <>
+      <p className="font-poppins text-sm">
+        En <span className="font-semibold">Fin de uso</span> especifica el uso que se le puede dar a este documento y a quién autorizas para dicho fin.
+      </p>
+      <p className="font-poppins text-sm">
+        En <span className="font-semibold">Validez</span> puedes indicar un periodo de tiempo por el cual quieras que se considere como válido la copia del documento al escanear el código QR.
+      </p>
+    </>
+  )
+}
