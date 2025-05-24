27a590bef9055ee826b656aac79f3acd1659a4d5
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-24 17:18:04 -0300

    Agregar ruta para el componente SecurePage en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home.jsx'
+import { SecurePage } from './pages/SecurePage.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/app' element={<SecurePage />} />
       </Routes>
     </BrowserRouter>
   )
