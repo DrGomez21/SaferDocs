bb87f3f993daf8ac62631f02c5a1a24e5b9a64eb
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-24 15:04:58 -0300

    Actualizar el componente Home para incluir un botón de carga de imagen con ícono

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,25 @@
 import { Logo } from "../components/Logo"
+import addFile from "../assets/addFile.svg"
 
 export const Home = () => {
   return (
     <>
-      <header className='w-full flex justify-center p-6 mb-4'>
+      <header className='w-full flex justify-center p-6'>
         <Logo />
       </header>
-      
+
       <main className="flex flex-col justify-center items-center gap-16 px-6">
         <section className="flex flex-col items-center gap-6">
           <p className="text-center text-2xl font-poppins">Protege tus 💳 <span className="font-semibold">documentos</span> de manera <span className="font-source-serif italic">fácil</span> y <span className="font-source-serif italic">segura</span></p>
 
           <div className="absolute -z-30 bg-blue-300 w-36 h-36 rounded-full blur-2xl" />
 
-          <button className="flex justify-center gap-2 w-full bg-primary text-white font-poppins font-semibold text-sm px-4 py-3 rounded-lg">
-            Elige una imagen
+          <input type="file" accept="image/*" className="hidden" id="imageInput" />
+          <button
+            className="flex justify-center items-center gap-2 w-full bg-primary text-white font-poppins font-semibold text-sm px-4 py-3 rounded-lg"
+            onClick={() => document.getElementById('imageInput').click()}
+          >
+            <img src={addFile} />Elige una imagen
           </button>
 
           <p className="font-poppins text-sm">
