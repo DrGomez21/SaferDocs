7878d5c0e849584612a947277d892dcee81afe9e
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-24 21:16:24 -0300

    Mejoras estéticas

--- src/components/Explicacion.jsx
+++ src/components/Explicacion.jsx
@@ -1,10 +1,10 @@
 export const Explicacion = () => {
   return (
     <>
-      <p className="font-poppins text-sm">
+      <p className="font-poppins text-sm max-w-md">
         En <span className="font-semibold">Fin de uso</span> especifica el uso que se le puede dar a este documento y a quién autorizas para dicho fin.
       </p>
-      <p className="font-poppins text-sm">
+      <p className="font-poppins text-sm max-w-md">
         En <span className="font-semibold">Validez</span> puedes indicar un periodo de tiempo por el cual quieras que se considere como válido la copia del documento al escanear el código QR.
       </p>
     </>
